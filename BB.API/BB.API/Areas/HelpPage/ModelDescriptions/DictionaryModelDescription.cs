74263e30e838eeed847e8e6cf06c930868a62055
vitorfwt <vitorfwt@gmail.com>
2016-08-19 20:45:37 -0300

    Commit inicial

---
+++ BB.API/BB.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BB.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BB.API/BB.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BB.API.WebApiApplication" Language="C#" %>
